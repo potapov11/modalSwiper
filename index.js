758fc6eafd8857c8809f7752991cffc7e3b2e1ce
s.potapov <s.potapov@severnaya.ru>
2024-07-05 13:46:17 +0300

    z

--- index.js
+++ index.js
@@ -1,71 +1,84 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let swiper;
+document.addEventListener("DOMContentLoaded", function () {
+  let swiper;
 
-    function initializeSwiper() {
-        const isMobile = window.matchMedia('(max-width: 758px)').matches;// булевое значение(Метод matches возвращает true, если текущая ширина экрана соответствует медиа-запросу, и false в противном случае. )
+  function initializeSwiper() {
+    const isMobile = window.matchMedia("(max-width: 758px)").matches; // булевое значение(Метод matches возвращает true, если текущая ширина экрана соответствует медиа-запросу, и false в противном случае. )
 
-        swiper = new Swiper('.swiper', {
-            speed: 300,
-            slidesPerView: 1,
-            centeredSlides: true,
-            centeredSlidesBounds: true,
-            loop: true,
-            lazy: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            zoom: {
-                maxRatio: 3, // Максимальный зум
-                minRatio: 1, // Минимальный зум
-                enabled: isMobile, // Включаем зум только на мобильных устройствах
-                limitToOriginalSize: true,
-            },
-        });
-    }
+    const swiper = new Swiper(".swiper", {
+      // Setting default settings
+      grabCursor: true,
+      centeredSlides: true,
+      loop: true,
+      // Setting minimum and maximum zoom ration
+      zoom: {
+        maxRatio: 1.2,
+        minRation: 1,
+      },
+    });
 
-    initializeSwiper();
+    // Use built in zoom.in() and zoom.out() function to scale images
+    // When slide starts to change slideChangeTransitionStart event fires and we use it to scale down the image.
+    swiper.on("slideChangeTransitionStart", swiper.zoom.out);
+    // swiper = new Swiper(".swiper", {
+    //   speed: 300,
+    //   slidesPerView: 1,
+    //   centeredSlides: true,
+    //   centeredSlidesBounds: true,
+    //   loop: true,
+    //   lazy: true,
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //   },
+    //   navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
+    //   zoom: {
+    //     maxRatio: 3, // Максимальный зум
+    //     minRatio: 0, // Минимальный зум
+    //     enabled: isMobile, // Включаем зум только на мобильных устройствах
+    //     limitToOriginalSize: true,
+    //   },
+    // });
+  }
+
+  initializeSwiper();
 });
 
-
-
 // Инициализация модального окна
-document.addEventListener('DOMContentLoaded', function() {
-    const modalElements = document.querySelectorAll('.js-modal');
-    window.modals = {};
+document.addEventListener("DOMContentLoaded", function () {
+  const modalElements = document.querySelectorAll(".js-modal");
+  window.modals = {};
 
-    modalElements.forEach(function(modal) {
-        const modalId = modal.getAttribute('data-modal-id');
-        if (modalId) {
-            window.modals[modalId] = {
-                element: modal,
-                close: function() {
-                    modal.classList.remove('modal--active');
-                    modal.setAttribute('aria-hidden', 'true');
-                }
-            };
-        }
-    });
+  modalElements.forEach(function (modal) {
+    const modalId = modal.getAttribute("data-modal-id");
+    if (modalId) {
+      window.modals[modalId] = {
+        element: modal,
+        close: function () {
+          modal.classList.remove("modal--active");
+          modal.setAttribute("aria-hidden", "true");
+        },
+      };
+    }
+  });
 });
 
 // Определение функции modalClose()
 function modalClose(id) {
-    if (window.modals && window.modals[id]) {
-        window.modals[id].close();
-    }
+  if (window.modals && window.modals[id]) {
+    window.modals[id].close();
+  }
 }
 
 // Привязка функции modalClose() к кнопкам закрытия
-const modalCloseButtons = document.querySelectorAll('.js-modal-close-btn');
-modalCloseButtons.forEach(function(btn) {
-    const modalId = btn.closest('.js-modal').getAttribute('data-modal-id');
-    btn.addEventListener('click', function() {
-        modalClose(modalId);
-    });
+const modalCloseButtons = document.querySelectorAll(".js-modal-close-btn");
+modalCloseButtons.forEach(function (btn) {
+  const modalId = btn.closest(".js-modal").getAttribute("data-modal-id");
+  btn.addEventListener("click", function () {
+    modalClose(modalId);
+  });
 });
 
 /*
@@ -79,4 +92,3 @@ function modalOpen(id, trigger = false) {
     }
 }
 */
-
